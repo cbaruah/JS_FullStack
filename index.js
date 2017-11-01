e81017fb14241d2719f2c338b77663322b4b6a6b
cbaruah <cbaruah@ncsu.edu>
2017-11-01 16:27:30 -0400

    added google oauth and passport for authentication

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+require('./services/passport.js');
+const authRoutes = require('./routes/authRoutes');
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
-});
+authRoutes(app);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT);
