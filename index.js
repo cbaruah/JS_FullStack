4cb86d79eddead1e02524a00156ca4c2dd439924
cbaruah <cbaruah@ncsu.edu>
2017-11-02 12:50:01 -0400

    added models and mongoDB config

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+require('./models/User');
 require('./services/passport.js');
 const authRoutes = require('./routes/authRoutes');
 
+mongoose.connect(keys.mongoURI);
 const app = express();
 
 authRoutes(app);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
