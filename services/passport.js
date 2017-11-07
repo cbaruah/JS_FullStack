6792aab768050102fe0cd7f98aa16908e4e53853
cbaruah <cbaruah@ncsu.edu>
2017-11-07 13:40:36 -0500

    Session and authentication

--- services/passport.js
+++ services/passport.js
@@ -5,12 +5,33 @@ const mongoose = require('mongoose');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then(user => {
+        done(null, user);
+    });
+});
+
 passport.use(
     new GoogleStrategy({
         clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback'
-    }, (accessToken, refreshToken, profile, done) => {
-        new User({googleID: profile.id}).save();
+    }, 
+    (accessToken, refreshToken, profile, done) => {
+        User.findOne({ googleID: profile.id}).then((existingUser) => {
+            if(existingUser){
+                // already have a record with given profile ID
+                done(null, existingUser);
+            }
+            else{
+                //create a new record
+                new User({googleID: profile.id}).save().then(user => done(null, user));
+
+            }
+        });
     })
 );
